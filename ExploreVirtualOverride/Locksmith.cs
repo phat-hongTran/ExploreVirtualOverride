fe59de341d9dd9dc884d909aee4e3fd2745ae410
Phat Tran <ptran@strongtie.com>
2024-01-07 14:43:56 +0700

    Add project files.

---
+++ ExploreVirtualOverride/Locksmith.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExploreVirtualOverride
+{
+    class Locksmith
+    {
+        public string Combination { private get; set; }
+
+        public void OpenSafe(Safe safe, SafeOwner owner)
+        {
+            safe.PickLock(this);
+            string safeContents = safe.Open(Combination);
+            ReturnContents(safeContents, owner);
+        }
+
+        protected void ReturnContents(string safeContents, SafeOwner owner)
+        {
+            owner.ReceiveContents(safeContents);
+        }
+    }
+}
---
+++ ExploreVirtualOverride/SafeOwner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploreVirtualOverride
+{
+    class SafeOwner
+    {
+        private string valuables = "";
+
+        public void ReceiveContents(string safeContents)
+        {
+            valuables = safeContents;
+            Console.WriteLine($"Thank you for returning my {valuables}!");
+        }
+    }
+}
